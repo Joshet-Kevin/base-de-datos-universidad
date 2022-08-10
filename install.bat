da37aad814c09c707b4fd128607f9ea2acc527da
Joshet-Kevin <mymshikyn8@gmail.com>
2022-08-10 00:22:03 -0500

    Revert "Tarea 1 - Desarrollo de Plataforma"
    This reverts commit 5cfbb9f15051f6a61eb0a412eb5f8a2edae880bc.

--- install.bat
+++
@@ -1,7 +0,0 @@
-echo Instalador de la base de datos Universidad
-echo Autor: Joshet Ccoyore
-echo Fecha: 8/9/2022
-sqlcmd -Slocalhost\SQLEXPRESS -E -i BDUniversidad.sql
-sqlcmd -Slocalhost\SQLEXPRESS -E -i BDUniversidadPA.sql
-echo Se ejecuto correctamente la base de datos
-pause
